1add181e43ea5c71d96fa1a8b6df2c64f29dc2bb
Rajesh1308 <rajesharulrathna@gmail.com>
2023-01-13 16:49:28 +0530

    Sleeping for 30 minutes

--- pysleep.py
+++ pysleep.py
@@ -3,6 +3,10 @@ import os
 
 #rundll32.exe powrprof.dll,SetSuspendState 0,1,0
 
+#Reference
+#https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/rundll32
+#https://learn.microsoft.com/en-us/windows/win32/api/powrprof/nf-powrprof-setsuspendstate
+
 while True:
     time.sleep(1800)
     os.system('rundll32.exe powrprof.dll,SetSuspendState sleep')
